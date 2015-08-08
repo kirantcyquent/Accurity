9d72dae4064430eedbfb653bdaec252c99421d6b
unknown <SCI00296@SCI-219.SCI.COM>
2015-08-08 18:46:53 +0530

    CHG: Add log

--- viewlog.php
+++ viewlog.php
@@ -1,12 +1,15 @@
 <?php
 $dir    = '/tmp/logs1/';
 $files1 = scandir($dir);
-echo "<pre>"; print_r($files1);
-echo "<table>";
+echo "<table border='1'><tr><td>SeqNo</td><td>FileName</td></tr>";
+$count = 0;
 foreach($files1 as $file)
 {
-	 if (!in_array($file,array(".",".."))) 
-		echo "<tr><td><a href='dispfile.php?filename=$file'>$file</a></td></tr>";
+	 if (!in_array($file,array(".","..")))
+	 {
+		$count++;
+		echo "<tr><td>$count</td><td><a href='dispfile.php?filename=$file'>$file</a></td></tr>";
+	 }
 }
 echo "</table>";
 
