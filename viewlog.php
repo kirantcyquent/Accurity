a539b1d32b82dea64f95e03a37ff9a1b14d4a56c
unknown <SCI00296@SCI-219.SCI.COM>
2015-08-08 16:51:52 +0530

    CHG: Add log

--- viewlog.php
+++ viewlog.php
@@ -1,9 +1,10 @@
 <?php
 $dir    = '/tmp/logs1/';
 $files1 = scandir($dir);
+echo "<pre>"; print_r($files1);
 echo "<table><tr><td>";
 foreach($files1 as $file)
-echo "<a href='dispfile.php?filename=$file'>Visit our HTML tutorial</a></td>";
+echo "<a href='dispfile.php?filename=$file'>$file</a></td>";
 echo "</tr></table>";
 
 
