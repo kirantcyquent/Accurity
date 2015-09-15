14dfa049ef7a67b3a0ed37e61a62c1640eaf03ad
kirantcyquent <kiran.t@cyquent.com>
2015-09-15 12:49:56 +0530

    Updated as per new values
    Updated as per new values

--- classes/config.php
+++ classes/config.php
@@ -1,13 +1,13 @@
 <?php
 
-define('DB_USER', 'root');
-define('DB_PASS', 'secret');
+define('DB_USER', 'loan_user');
+define('DB_PASS', 'l0@n');
 
-$dbhost = 'localhost';
+$dbhost = 'Db1-hf.realtytrac.com';
 $dbname = 'loan';
 
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'loan');
+define('DB_HOST', $dbhost);
+define('DB_NAME', $dbname);
 define('DB_URI', "mysql:host=$dbhost;dbname=$dbname");
 
 ?>
